3eb437123b2a9a50c9398215366fd6acfc72b054
Fakrul Islam <fakrulislam0085@gmail.com>
2025-05-24 20:06:59 +0200

    Update 4_stl_pair.cpp

--- Week 1/Module 1/4_stl_pair.cpp
+++ Week 1/Module 1/4_stl_pair.cpp
@@ -3,8 +3,12 @@ using namespace std;
 int main()
 {
     pair <string, int> p;
-    p = make_pair("Fariha", 06);
+    p = make_pair("Fariha", 06);  //It is defined in #include <utility> header
     cout << p.first << " " << p.second << endl;
+    
+    pair <int, int> p2; 
+    p2 = make_pair(6, 7); 
+    cout << p2.first << " " << p2.second << endl; 
     
     return 0;
 }
