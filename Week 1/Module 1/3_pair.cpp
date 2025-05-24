b7e7b0715896f7642986d6c95fbde6dd56db4788
Fakrul Islam <fakrulislam0085@gmail.com>
2025-05-24 20:08:06 +0200

    Update 3_pair.cpp

--- Week 1/Module 1/3_pair.cpp
+++ Week 1/Module 1/3_pair.cpp
@@ -14,23 +14,13 @@ class myPair
 int main()
 {
     myPair p;
-    //1st
-    // p.first = 10;
-    // p.second = 30;
+    //1. initialize here
+    // p.f = 10;
+    // p.s = 30;
 
-   //or call our function
+    //2. or call our function
     p.make_pair(10,20);
     cout << p.f << " " << p.s << endl;
-
-
-    /*STL pair syntax: pair<datatype, datatype> variable; 
-    After declaring our pair we can print the value as variable.first and variable.second 
-    we can initialize pair in two ways */
-
-    pair <int, string> p2(2, "graph");  //1st
-    pair <string, int> p3;  //2nd
-    p3 = {"tree", 5}; 
-    cout << p2.first << " " << p2.second << endl << p3.first << " " << p3.second << endl; 
-
+    
     return 0;
 }
