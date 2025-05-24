0a4f6ccd0fd282f8b74c4e9736470b7db8404bf8
Fakrul Islam <fakrulislam0085@gmail.com>
2025-05-24 20:09:34 +0200

    Update 4_stl_pair.cpp

--- Week 1/Module 1/4_stl_pair.cpp
+++ Week 1/Module 1/4_stl_pair.cpp
@@ -2,13 +2,25 @@
 using namespace std;
 int main()
 {
+    
+    
+    
+    /*STL pair syntax: pair<datatype, datatype> variable; 
+    After declaring our pair we can print the value as variable.first and variable.second 
+     */
     pair <string, int> p;
     p = make_pair("Fariha", 06);  //It is defined in #include <utility> header
     cout << p.first << " " << p.second << endl;
-    
+
     pair <int, int> p2; 
     p2 = make_pair(6, 7); 
     cout << p2.first << " " << p2.second << endl; 
+
+    //withour using make_pair() 
+    pair <int, string> p2(2, "graph");  //1st way
+    pair <string, int> p3;  //2nd way
+    p3 = {"tree", 5}; 
+    cout << p2.first << " " << p2.second << endl << p3.first << " " << p3.second << endl; 
     
     return 0;
 }
